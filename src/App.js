071a636676d6fd801bf8811c4dbf88ed7544c3e5
Rohit C <124383636+R021T@users.noreply.github.com>
2023-07-30 13:36:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import { TodoWrapper } from './components/TodoWrapper';
+import img from "./images/check.png"
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
      <div className='Head'>
       <h2>Todo App</h2>
      </div>
      <div className='Box'>
       <TodoWrapper/>
      </div>
+     <footer>
+      <img src={img}/>
+     </footer>
     </div>
   );
 }
